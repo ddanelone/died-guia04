b7c36128a23c9fdc5a66eee1f29dda1fb6fc9f7d
Diego Danelone <ddanelone@frsf.utn.edu.ar>
2023-03-30 18:21:22 -0300

    Ejercicio 1 casi terminado

--- src/ejercicio02/DetallePedido.java
+++ src/ejercicio02/DetallePedido.java
@@ -26,9 +26,7 @@ public class DetallePedido {
 	}
 	
 	public double totalLinea() {
-		//retorna el precio de esa línea del pedido. A su vez el pedido tiene un método que retorna el subtotal
-		//(el precio de cada línea de pedido) y el total, que es el subtotal mas el 21% de IVA en caso de que 
-		//el cliente sea consumidor final.
+		//retorna el precio de esa línea del pedido. 
 		return ((Double) this.producto.getPrecio() * this.cantidad);
 	}
 }
